eea4323aed684031b6e2bb4b2b5d6deb237ff938
Joel Southall <vextechmage@gmail.com>
2024-12-17 14:19:13 -0800

    Initial commit: Add Blazor Hex Grid Project

---
+++ HexGrid/HexMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HexGridProject.HexGrid
+{
+    public static class HexMath
+    {
+        // Function to calculate the pixel position of a hex tile
+        public static (float x, float y) GetHexPosition(int q, int r, float size)
+        {
+            float width = size * (float)Math.Sqrt(3); // Horizontal distance
+            float height = size * 1.5f;               // Vertical spacing for flat-top hexes
+
+
+            // Apply horizontal offset for even rows
+            float x = width * q + (r % 2) * (width / 2);
+            float y = height * r;
+
+            return (x, y);
+        }
+
+
+
+    }
+}
